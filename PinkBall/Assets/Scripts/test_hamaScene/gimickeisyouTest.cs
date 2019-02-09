a809d63734fad8c7b54094c11a1a7dcc0c9c3459
comp180906hamaguchi <comp180906pm.hamaguchi@gmail.com>
2019-02-09 19:54:59 +0900

    濱口初ブランチ　テスト

---
+++ PinkBall/Assets/Scripts/test_hamaScene/gimickeisyouTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gimickeisyouTest : GimicBase {
+
+    public void OnCollisionEnter(Collision other)
+    {
+        base.OnCollisionEnter(other);
+    }
+
+    void addForce(Collision other)
+    {
+        //base.addForce(other);
+    }
+
+
+}
