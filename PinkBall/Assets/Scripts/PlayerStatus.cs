9a14d674d4bd8ab2ad06e4cc1538b79236cd03ce
KT <k.tojima1118@gmail.com>
2019-02-13 14:38:35 +0900

    StageScene追加、PlayerStatus修正。

--- PinkBall/Assets/Scripts/PlayerStatus.cs
+++ PinkBall/Assets/Scripts/PlayerStatus.cs
@@ -5,13 +5,74 @@ using UnityEngine;
 public class PlayerStatus : MonoBehaviour
 {
     const int StartCount = 0;
-
     public int DefaultBallPoint = 3; // デフォルトの残機数
 
-    public int ball; // 玉の残機数
-    public int score; // プレイヤーのスコアを保存する
-    public int selectedCharacter; // 選択したキャラクターID
-    public int hitCounter; // キャラクターへのヒット数
+    private int ball; // 玉の残機数
+    private int score; // プレイヤーのスコアを保存する
+    private int selectedCharacter; // 選択したキャラクターID
+    private int hitCounter; // キャラクターへのヒット数
+
+    public int Ball
+    {
+        get
+        {
+            return ball;
+        }
+
+        set
+        {
+            if(value > 0)
+            {
+                if(DefaultBallPoint > ball)
+                {
+                    ball += value;
+                }
+            }
+            else
+            {
+                ball += value;
+            }
+        }
+    }
+
+    public int Score
+    {
+        get
+        {
+            return score;
+        }
+
+        set
+        {
+            score += value;
+        }
+    }
+
+    public int SelectedCharacter
+    {
+        get
+        {
+            return selectedCharacter;
+        }
+
+        set
+        {
+            selectedCharacter = value;
+        }
+    }
+
+    public int HitCounter
+    {
+        get
+        {
+            return hitCounter;
+        }
+
+        set
+        {
+            hitCounter = value;
+        }
+    }
 
     // Use this for initialization
     void Start()
@@ -20,45 +81,5 @@ public class PlayerStatus : MonoBehaviour
         score = StartCount;
         selectedCharacter = StartCount;
         hitCounter = StartCount;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    // スコアの追加を行う
-    public int UpdateScore(int ScorePoint)
-    {
-        score += ScorePoint; // スコアの追加
-        return score;
-    }
-
-    // ボール（残機）の増減を行う
-    public int LifePointUpDate(int managementBall)
-    {
-        // 残機数を増やす場合
-        if (managementBall > 0)
-        {
-            // 残機数の上限を設ける
-            if (ball < DefaultBallPoint)
-            {
-                ball += managementBall; // 残機増加
-            }
-        }
-        // 残機数を減らす場合
-        else
-        {
-            if (ball > 0)
-            {
-                ball += managementBall; // 残機減少
-            }
-            else
-            {
-                // ゲームオーバー
-            }
-        }
-        return ball;
     }
 }
---
+++ PinkBall/Assets/Scripts/StageTestScene_T/Warp.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : GimicBase 
+{
+    /// <summary>
+    /// Warp ball .
+    /// 動作確認はまだです。
+    /// </summary>
+
+    public enum WarpBallState
+    {
+        normal,
+        goToWarpPoint,
+    };
+
+    public GameObject ball; // 
+
+    private WarpBallState state;
+    private Vector3 velocity = Vector3.zero;
+    private InstantiateParticle instantiateParticle;
+
+    public Transform startPosition; // 転送元
+    public Transform destination; // 転送先
+
+    public float goToWaitPointSpeed;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        state = WarpBallState.normal;
+        instantiateParticle = GetComponent<InstantiateParticle>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (state == WarpBallState.goToWarpPoint)
+        {
+            GoToWarpWaitPoint();
+        }
+    }
+
+
+    void GoToWarpWaitPoint()
+    {
+        if (Vector3.Distance(transform.position, startPosition.position) > 0.1f
+        || Quaternion.Angle(transform.rotation, startPosition.rotation) >= 1f)
+        {
+            Debug.Log("移動中");
+            transform.position = Vector3.Lerp(transform.position, startPosition.position, goToWaitPointSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, startPosition.rotation, goToWaitPointSpeed * Time.deltaTime);
+        }
+        else if (transform.position != startPosition.position)
+        {
+            Debug.Log("きっちり位置と角度を合わせる");
+            transform.position = startPosition.position;
+            transform.rotation = startPosition.rotation;
+            //　ワープ用パーティクルの表示
+            instantiateParticle.InstantiateWarpParticle(transform.position);
+            //　0.5秒後にワープ
+            Invoke("WarpDestination", 0.5f);
+        }
+    }
+
+    // ワープさせる処理
+    void WarpDestination()
+    {
+        transform.position = destination.position; //
+        transform.rotation = destination.rotation; // 
+        instantiateParticle.InstantiateWarpParticle(transform.position); //　移動先でワープパーティクルの表示
+        SetState(WarpBallState.normal); // stateの変更
+    }
+
+    // state の変更
+    public void SetState(WarpBallState state, Transform waitPoint = null, Transform warpPoint = null)
+    {
+        this.state = state;
+        startPosition = waitPoint;
+        destination = warpPoint;
+        if (state == WarpBallState.goToWarpPoint)
+        {
+            velocity = Vector3.zero;
+        }
+    }
+}
+
+// ワープ先を設定する
+public class WarpArea : MonoBehaviour
+{
+    public Transform warpPoint; // 
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "ball")
+        {
+            // ballの状態をワープに変更
+            other.GetComponent<Warp>().SetState(
+                Warp.WarpBallState.goToWarpPoint,
+                transform,
+                warpPoint);
+        }
+    }
+}
+
+// パーティクルをインスタンス化
+public class InstantiateParticle : MonoBehaviour
+{
+    public GameObject warpParticle;
+
+    public void InstantiateWarpParticle(Vector3 pos)
+    {
+        Instantiate(warpParticle, pos, Quaternion.Euler(0f, 0f, 0f));
+    }
+}
+
+// パーティクルを自動で削除する
+public class DeleteParticle : MonoBehaviour
+{
+    public ParticleSystem destroyParticles; // パーティクルを指定
+    // Use this for initialization
+    void Start()
+    {
+        destroyParticles = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // パーティクルの再生が止まったらパーティクルを削除する
+        if (destroyParticles.isStopped)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
