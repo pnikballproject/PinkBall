681dd4609808cd4e3c43f5610c6587cf76dbaee4
tojima_k <st.tojima.18@gmail.com>
2019-02-13 20:59:31 +0900

    WarpPointのScriptを追加し、UISceneの修正を行いました。

---
+++ PinkBall/Assets/Scripts/StageTestScene_T/WarpPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpPoint : GimicBase
+{
+    public GameObject target;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 指定のGameObjectに転移させる
+        other.gameObject.transform.position = new Vector3(
+            target.transform.position.x,
+            target.transform.position.y,
+            target.transform.position.z
+            );
+    }
+}
