c4ca5abc4d14a0de637407e37611a765daeecf7a
tojima_k <st.tojima.18@gmail.com>
2019-02-14 19:03:37 +0900

    StageTestSceneの修正を行いました。

---
+++ PinkBall/Assets/Scripts/StageTestScene_T/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    public float speed;
+    public float radius;
+    public float yPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = radius * Mathf.Sin(Time.time * speed);
+        float y = yPosition;
+        float z = radius * Mathf.Cos(Time.time * speed);
+
+        transform.position = new Vector3(x, y, z);
+    }
+}
