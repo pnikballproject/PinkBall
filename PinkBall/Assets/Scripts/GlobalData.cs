6745519968c5144d9450b4181a153b7b53530959
comp180906hama <comp180906pm.hamaguchi@gmail.com>
2019-02-15 01:11:14 +0900

    2/15 GlobalDataを追加(static変数)しました。格納変数はキャラIDとスコアの一時保管用変数です

---
+++ PinkBall/Assets/Scripts/GlobalData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalData {
+
+    //ゲームスタート時に選択したキャラクターのID
+    public static int selectCharacterId;
+
+    //リザルトで表示するゲームスコアの受け渡し変数
+    public static int GlobalScore;
+
+	
+
+}
